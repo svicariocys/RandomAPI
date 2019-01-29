5455735b8ca139324aa20825f4f59ca767c731d9
Sebastián Vicario <svicario@cys.com.ar>
2019-01-29 12:52:50 -0300

    random api

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/participants', (req, res) =>{
+	let data = 
+		[
+			{
+				id: 0,
+				name: "Sebastian",
+				surname: "Vicario"
+			},
+			{
+				id: 1,
+				name: "Damian",
+				surname: "Ramonas"
+			}
+			
+		]
+	res.send(data);
+})
+
+app.get('/random', (req, res) =>{
+	let data = 
+	{
+		id: 0,
+		name: "Sebastian",
+		surname: "Vicario"
+	}
+	res.send(data);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
