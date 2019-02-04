80d661e70257e110a41f1944ef1897bd0963b739
svicario <svicario@cys.com.ar>
2019-02-04 15:51:22 -0300

    post location

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const ConnectorDB = require('./db/connectorDB')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.get('/', (req, res) => {
 	ConnectorDB.connect()
 	.then(result => {
@@ -47,5 +51,16 @@ app.get('/random', (req, res) =>{
 		res.status(404).send('connection error', err.stack);
 	})
 })
+
+
+app.post('/location', (req, res) =>{
+	let latitude = req.body.latitude;
+  let longtitude = req.body.longtitude;
+
+  res.status(200).send("Latitude: "+latitude+" Longitude: "+longtitude);
+})
+
+
+
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT || port}!`))
